fe5c9b8de9e361b909a19e75d3254cf51ff047a3
Melina Mejía Bedoya <Melina Meja Bedoya>
2017-04-30 12:33:12 -0500

    Se crea el juego FizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,1 +1,14 @@
+var i = 0
+
+for (i=1; i<=100; i++) {
+	if (i%3 == 0) {
+		document.write('Fizz')
+	} else if (i%5 == 0) {
+		document.write('Buzz')
+	} else if (i%3 == 0 && i%5 == 0) {
+		document.write('FizzBuzz')
+	} else {
+		document.write(i)
+	}
+}
 
