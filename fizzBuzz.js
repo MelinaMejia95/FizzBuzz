3c8a5fe58e6ff636c7848202e85547c9965fe17c
MelinaMejia95 <melinamejia95@gmail.com>
2017-04-30 12:57:35 -0500

    Ultimos cambios

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,13 +2,14 @@ var i = 0
 
 for (i=1; i<=100; i++) {
 	if (i%3 == 0) {
-		document.write('Fizz')
+		document.write('<br /> Fizz')
 	} else if (i%5 == 0) {
-		document.write('Buzz')
+		document.write('<br /> Buzz')
 	} else if (i%3 == 0 && i%5 == 0) {
-		document.write('FizzBuzz')
+		document.write('<br /> FizzBuzz')
 	} else {
-		document.write(i)
+		document.write('<br /> ' + i)
 	}
 }
+
 
